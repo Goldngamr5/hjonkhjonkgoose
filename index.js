15ee2d9be222c82f3d4bc8cd79857d4859948ca2
Goldngamr5 <awsomejojop@gmail.com>
2020-09-08 14:48:05 -0700

    make it better

---
+++ index.js
@@ -0,0 +1,331 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on("message", (msg) => {
+  if (msg.content === "this is going to take a while") {
+    msg.reply("yes it is")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content.startsWith === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("ping")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "that was a while") {
+    msg.reply("yes it was")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "is it done") {
+    msg.reply("yes")
+  }
+})
+client.on("message", (msg) => {
+  if (msg.content === "finally") {
+    msg.reply("yep")
+  }
+})
+client.login("NzUyNjcwMzc3Njk5ODM1OTQ1.X1bA6Q.a1sfeb_Id07U58_9hu6Lr1F1NZM")
