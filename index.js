c7eda37015ffe0494136df0b425243b3e403b5a9
Goldngamr5 <awsomejojop@gmail.com>
2020-09-08 17:19:42 -0700

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const Discord = require("discord.js")
+const fs = require("fs")
 const client = new Discord.Client()
+require("dotenv").config()
+
+fs.readdir("./events/", (err, files) => {
+    files.forEach((file) => {
+        const eventHandler = require(`./events/${file}`)
+        const eventName = file.split(".")[0]
+        client.on(eventName, (...args) => eventHandler(client, ...args))
+    })
+})
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
