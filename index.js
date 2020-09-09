8a2d940ec522d0d713c53638ea91c09b4183a3f5
Goldngamr5 <70986642+Goldngamr5@users.noreply.github.com>
2020-09-08 18:07:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js")
 const fs = require("fs")
 const client = new Discord.Client()
-require("dotenv").config()
 
 fs.readdir("./events/", (err, files) => {
     files.forEach((file) => {
